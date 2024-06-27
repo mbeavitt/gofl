60c94a7df7a6af7baf6d88c8723947bfb4478032
Michael Beavitt <mbeavitt@gmail.com>
2024-06-27 01:48:10 +0100

    Used raylib to migrate this terminal program to a windowed program :)

---
+++ README.md
@@ -0,0 +1,12 @@
+## The Game of Life
+
+This is a simple implementation of Conway's "The Game of Life" in the C programming language.
+It is written for installation and execution on Unix operating systems.
+
+Installation:
+
+1)
+`git clone https://github.com/mbeavitt/gofl/`
+
+2)
+`cd gofl && make`
--- main.c
+++ main.c
@@ -1,45 +1,69 @@
+#include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
-#define BLANK ' '
+#define PADDING 2
+#define CUBESIZE 10
+#define NAVBAR_OFFSET 5
+#define BLANK_CHAR ' '
 
+//------------------------------------------------------------------------------------
+// Function prototypes
+//------------------------------------------------------------------------------------
 void initialise_random(int num_rows, int num_cols, char board[num_rows][num_cols], char input);
 void print_board(int num_rows, int num_cols, char board[num_rows][num_cols]);
 void game_step(int num_rows, int num_cols, char board[num_rows][num_cols], char key);
 
-int main(int argv, char **argc) {
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void) {
+    const int screenWidth = 1440;
+    const int screenHeight = 900;
+    int num_rows = screenWidth / (CUBESIZE);
+    int num_cols = -NAVBAR_OFFSET + screenHeight / (CUBESIZE);
+    char board[num_rows][num_cols];
 
     srand((unsigned) time(NULL));
-    int num_rows, num_cols, speed;
 
-    switch (argv) {
-        case 1: printf("Usage: ./main rows cols [speed]\n"); exit(EXIT_FAILURE);
-        case 2: num_rows = atoi(argc[1]); num_cols = atoi(argc[1]); speed = 3; break;
-        case 3: num_rows = atoi(argc[1]); num_cols = atoi(argc[2]); speed = 3; break;
-        case 4: num_rows = atoi(argc[1]); num_cols = atoi(argc[2]); speed = atoi(argc[3]); break;
-        default: printf("ERROR: Too many arguments\n"); exit(EXIT_FAILURE);
-    }
-    
-    char board[num_rows][num_cols];
+    InitWindow(screenWidth, screenHeight, "The Game of Life");
     initialise_random(num_rows, num_cols, board, 'o');
-    print_board(num_rows, num_cols, board);
-    for (;;) {
-        system("clear");
-        game_step(num_rows, num_cols, board, 'o');
-        print_board(num_rows, num_cols, board);
-        usleep(speed * 10000);
+
+    SetTargetFPS(10);               // Set our game to run at 10 frames-per-second
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+
+        // Draw
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+            // draw board from 'board' variable
+            print_board(num_rows, num_cols, board);
+            game_step(num_rows, num_cols, board, 'o');
+
+        EndDrawing();
     }
+
+    CloseWindow();        // Close window and OpenGL context
+
+    return 0;
 }
 
+
+//------------------------------------------------------------------------------------
+// Function definitions 
+//------------------------------------------------------------------------------------
 void initialise_random(int num_rows, int num_cols, char board[num_rows][num_cols], char input) {
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
             if ((rand() % 5) == 0) {
-                board[i][j] = input; 
+                board[i][j] = input;
             } else {
-                board[i][j] = BLANK;
+                board[i][j] = ' ';
             }
         }
     }
@@ -48,10 +72,15 @@ void initialise_random(int num_rows, int num_cols, char board[num_rows][num_cols
 void print_board(int num_rows, int num_cols, char board[num_rows][num_cols]) {
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }        
+            if (board[i][j] == 'o') {
+                DrawRectangle(i * CUBESIZE,
+                             j * CUBESIZE,
+                             CUBESIZE - PADDING,
+                             CUBESIZE - PADDING,
+                             WHITE);
+            }
+        } 
+    }
 }
 
 void game_step(int num_rows, int num_cols, char board[num_rows][num_cols], char key) {
@@ -75,15 +104,15 @@ void game_step(int num_rows, int num_cols, char board[num_rows][num_cols], char 
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
             switch (temp_board[i][j]) {
-                case 0: board[i][j] = BLANK; break;
-                case 1: board[i][j] = BLANK; break;
+                case 0: board[i][j] = BLANK_CHAR; break;
+                case 1: board[i][j] = BLANK_CHAR; break;
                 case 2: if (board[i][j] == key) board[i][j] = key; break;
                 case 3: board[i][j] = key; break;
-                case 4: board[i][j] = BLANK; break;
-                case 5: board[i][j] = BLANK; break;
-                case 6: board[i][j] = BLANK; break;
-                case 7: board[i][j] = BLANK; break;
-                case 8: board[i][j] = BLANK; break;
+                case 4: board[i][j] = BLANK_CHAR; break;
+                case 5: board[i][j] = BLANK_CHAR; break;
+                case 6: board[i][j] = BLANK_CHAR; break;
+                case 7: board[i][j] = BLANK_CHAR; break;
+                case 8: board[i][j] = BLANK_CHAR; break;
             }
         }
     }
